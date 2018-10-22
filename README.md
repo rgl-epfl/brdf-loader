20ddfd4cc90b0916c8ea10053f3c64eb02fe0079
Wenzel Jakob <wenzel.jakob@epfl.ch>
2018-10-22 12:28:42 +0200

    Python plotting code

--- README.md
+++ README.md
@@ -1,40 +1,45 @@
 # BRDF Loader
 
-This repository provides a self-contained implementation of the
-evaluation and sampling code for the paper
+This repository contains a self-contained implementation of evaluation and
+sampling code for the paper
 
 > *An Adaptive Parameterization for Efficient Material
 >  Acquisition and Rendering*
 >
 > by Jonathan Dupuy and Wenzel Jakob
 
-This file only contains the interface. To also compile
-the implementation, specify
+A Mitsuba plugin and Python routines for plotting are also provided.
+
+
+## Evaluation and sampling code
+
+A header file implementation of the model can be found in ``powitacq.h`` /
+``powitacq.inl`` (for spectral files) and ``powitacq_rgb.h`` /
+``powitacq_rgb.inl`` (for RGB files) Note that only the interface part is
+enabled by default; to also compile the implementation, specify
 
 ```cpp
 #define POWITACQ_IMPLEMENTATION 1
 ```
 
-before including this header file. The namespace ``powitacq``
-refers to the internal name of the project ("acquisition
+before including this the corersponding ``.h`` file. The namespace
+``powitacq`` refers to the internal name of the project ("acquisition
 using power iterations).
 
-This proof-of-concept implementation involves some
-inefficiencies that should be removed in a
-"production" setting.
+This proof-of-concept implementation involves some inefficiencies that should
+be removed in a "production" setting.
 
-1. It returns the full captured spectrum using a
-   ``std::valarray``, which causes dynamic
-   memory allocation at every BRDF evaluation.
+1. The spectral verision returns captured spectrum using a ``std::valarray``,
+   which causes dynamic memory allocation at every BRDF evaluation.
 
-   In practice, the rendering system may only want
-   to evaluate a fixed subset of the wavelengths,
-   which could furthermore be stored on the stack.
+   In practice, the rendering system may only want to evaluate a fixed subset
+   of the wavelengths, which could furthermore be stored on the stack.
 
-2. The implementation doesn't rely on vectorization
-   to accelerate simultaneous evaluation at multiple
-   wavelengths.
+2. The implementation doesn't rely on vectorization to accelerate simultaneous
+   evaluation at multiple wavelengths.
 
-In addition, we also provide a loader for our BRDF conversions
-from spectral to RGB (see powitacq_rgb.h). The interface and use
-is the same as that provided in powitacq.h.
+## Python loader
+
+The ``python`` directory contains functionality to load and save ``.bsdf``
+files via Python/NumPy. The file ``visualize.py`` loads an RGB material file,
+plots the VNDF and slice data, and then writes it back.
---
+++ python/visualize.py
@@ -0,0 +1,189 @@
+import numpy as np
+import struct
+import os
+
+def size_fmt(num, suffix='B'):
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi']:
+        if abs(num) < 1024.0:
+            return "%3.1f %s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f %s%s" % (num, 'Yi', suffix)
+
+def read_tensor(filename):
+    with open(filename, 'rb') as f:
+        def unpack(fmt):
+            result = struct.unpack(fmt, f.read(struct.calcsize(fmt)))
+            return result if len(result) > 1 else result[0]
+
+        if f.read(12) != 'tensor_file\0'.encode('utf8'):
+            raise Exception('Invalid tensor file (header not recognized)')
+
+        if unpack('<BB') != (1, 0):
+            raise Exception('Invalid tensor file (unrecognized file format version)')
+
+        field_count = unpack('<I')
+        size = os.stat(filename).st_size
+        print('Loading tensor data from \"%s\" .. (%s, %i field%s)' \
+            % (filename, size_fmt(size), field_count, 's' if field_count > 1 else ''))
+
+        # Maps from Struct.EType field in Mitsuba
+        dtype_map = {
+            1: np.uint8,
+            2: np.int8,
+            3: np.uint16,
+            4: np.int16,
+            5: np.uint32,
+            6: np.int32,
+            7: np.uint64,
+            8: np.int64,
+            9: np.float16,
+            10: np.float32,
+            11: np.float64
+        }
+
+        fields = {}
+        for i in range(field_count):
+            field_name = f.read(unpack('<H')).decode('utf8')
+            field_ndim = unpack('<H')
+            field_dtype = dtype_map[unpack('<B')]
+            field_offset = unpack('<Q')
+            field_shape = unpack('<' + 'Q'*field_ndim)
+            fields[field_name] = (field_offset, field_dtype, field_shape)
+
+        result = {}
+        for k, v in fields.items():
+            f.seek(v[0])
+            result[k] = np.fromfile(f, dtype=v[1], count=np.prod(v[2])).reshape(v[2])
+    return result
+
+def write_tensor(filename, align=8, **kwargs):
+    with open(filename, 'wb') as f:
+        # Identifier
+        f.write('tensor_file\0'.encode('utf8'))
+
+        # Version number
+        f.write(struct.pack('<BB', 1, 0))
+
+        # Number of fields
+        f.write(struct.pack('<I', len(kwargs)))
+
+        # Maps to Struct.EType field in Mitsuba
+        dtype_map = {
+            np.uint8 : 1,
+            np.int8 : 2,
+            np.uint16 : 3,
+            np.int16 : 4,
+            np.uint32 : 5,
+            np.int32 : 6,
+            np.uint64 : 7,
+            np.int64 : 8,
+            np.float16: 9,
+            np.float32: 10,
+            np.float64: 11
+        }
+
+        offsets = {}
+        fields = dict(kwargs)
+
+        # Write all fields
+        for k, v in fields.items():
+            if type(v) is str:
+                v = np.frombuffer(v.encode('utf8'), dtype=np.uint8)
+            else:
+                v = np.ascontiguousarray(v)
+            fields[k] = v
+
+            # Field identifier
+            label = k.encode('utf8')
+            f.write(struct.pack('<H', len(label)))
+            f.write(label)
+
+            # Field dimension
+            f.write(struct.pack('<H', v.ndim))
+
+            found = False
+            for dt in dtype_map.keys():
+                if dt == v.dtype:
+                    found = True
+                    f.write(struct.pack('B', dtype_map[dt]))
+                    break
+            if not found:
+                raise Exception("Unsupported dtype: %s" % str(v.dtype))
+
+            # Field offset (unknown for now)
+            offsets[k] = f.tell()
+            f.write(struct.pack('<Q', 0))
+
+            # Field sizes
+            f.write(struct.pack('<' + ('Q' * v.ndim), *v.shape))
+
+        for k, v in fields.items():
+            # Set field offset
+            pos = f.tell()
+
+            # Pad to requested alignment
+            pos = (pos + align - 1) // align * align
+
+            f.seek(offsets[k])
+            f.write(struct.pack('<Q', pos))
+            f.seek(pos)
+
+            # Field data
+            v.tofile(f)
+
+        print('Wrote \"%s\" (%s)' % (filename, size_fmt(f.tell())))
+
+
+def decode_string(a):
+    return a.view('S%i' % a.size)[0].decode('utf8')
+
+def to_srgb(v):
+    return np.where(v < 0.0031308, v * 12.92, 1.055 * (v**(1/2.4)) - 0.055)
+
+def plot_tensor(tensor, size=1, normalize_each=True, scale=1, invalid=None):
+    import matplotlib.pyplot as plt
+    if tensor.ndim == 4:
+        tensor = tensor[:, :, np.newaxis, :, :]
+    tensor = np.moveaxis(tensor, [0, 1, 2, 3, 4], [1, 0, 4, 2, 3])
+    tensor_max = None if normalize_each else np.max(tensor)
+
+    fig, ax = plt.subplots(tensor.shape[1], tensor.shape[0],
+                           figsize = (tensor.shape[0]*size, tensor.shape[1]*size))
+    for i in range(tensor.shape[0]):
+        for j in range(tensor.shape[1]):
+            img = tensor[i, j, ...]
+            img_max = np.max(img) if normalize_each else tensor_max
+            axc = ax[i] if tensor.shape[1] == 1 else ax[j][i] 
+            tonemapped = to_srgb(np.clip(img * (scale / np.maximum(1e-10, img_max)), 0, 1))
+            if invalid is not None:
+                tonemapped[invalid[j, i]] = [1, 0, 0]
+            axc.imshow(tonemapped.squeeze(), interpolation='nearest',
+                       clim=(0, 1), extent=[0,1,0,1], aspect=1)
+            axc.get_xaxis().set_visible(False)
+            axc.get_yaxis().set_visible(False)
+
+    axc = ax[0] if tensor.shape[1] == 1 else ax[0][0]      
+    axc.annotate('$\\theta_i$', xy=(2, 1.15), xytext=(0, 1.1),
+                 textcoords='axes fraction', xycoords='axes fraction', zorder=100,
+                 arrowprops=dict(arrowstyle="->", connectionstyle="arc3"))
+    if tensor.shape[1] > 1:
+        axc.annotate('$\\phi_i$', xy=(-0.17, -1), xytext=(-0.25, .85),
+                     textcoords='axes fraction', xycoords='axes fraction', zorder=100,
+                     arrowprops=dict(arrowstyle="->", connectionstyle="arc3"))
+
+    fig.tight_layout()
+    return fig
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    # Read a tensor file from disk
+    tensor = read_tensor("morpho_melenaus_rgb.bsdf")
+    print("Description: %s" % decode_string(tensor['description']))
+    print("Available fields: %s" % str(list(tensor.keys())))
+
+    plot_tensor(tensor['vndf'])
+    plot_tensor(tensor['rgb'], invalid=(tensor['luminance'] == 0))
+    plt.show()
+
+    # Write the tensor file again (the output should be identical)
+    write_tensor("out.bsdf", **tensor)
